9859ecb1747def3406fc09cf382681d60a0492d0
Sandiru Samarakoon <110292904+Sandiru@users.noreply.github.com>
2024-04-29 18:07:36 +0530

    Add files via upload

---
+++ src/components/navigationBar.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const navigationBar = () => {
+  return (
+    <View>
+
+
+      <Text>navigationBar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default navigationBar
