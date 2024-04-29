362bd12708c4b54bc3d973d0229a752b5146687a
Sandiru Samarakoon <110292904+Sandiru@users.noreply.github.com>
2024-04-29 18:10:25 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Login from './src/screens/Login';
+
+function App(): React.JSX.Element {
+ 
+
+  return (
+  <Login/>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
